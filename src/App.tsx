fa4a44629d5b31390528ef65e6099d11e7edb214
Przemyslaw Kaminski <przemo098@hotmail.com>
2020-09-23 16:58:41 +0200

    add recharts lib and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {LineChart, Line, XAxis, YAxis} from 'recharts';
+
+const rand = 300;
+const data: Array<any> = [];
+for (let i = 0; i < 7; i++) {
+    let d = {
+        year: 2000 + i,
+        value: Math.random() * (rand + 50) + 100
+    };
+
+    data.push(d);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Advertising Data ETL-V
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <div >
+            <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            >
+                <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
+                <XAxis dataKey="year" />
+                <YAxis />
+            </LineChart>
+        </div>
     </div>
   );
 }
+
+
 
 export default App;
